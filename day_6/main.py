3ebdf9066b8b09205abaa54cdbbd4480f0729fa0
Bonsa11 <sambodza@hotmail.co.uk>
2022-12-06 16:35:03 +0000

    day 6

---
+++ day_6/main.py
@@ -0,0 +1,28 @@
+
+def part1():
+    with open('./input.txt') as f:
+        data = f.read()
+
+
+    lp = 0
+    while lp < len(data):
+        if len(set(data[lp:lp +4])) == 4:
+            break
+        else:
+            lp += 1
+
+    print(lp +4)
+
+if __name__ == '__main__':
+    with open('./input.txt') as f:
+        data = f.read()
+
+    lp = 0
+    while lp < len(data):
+        if len(set(data[lp:lp + 14])) == 14:
+            print(set(data[lp:lp + 14]))
+            break
+        else:
+            lp += 1
+
+    print(lp + 14)
